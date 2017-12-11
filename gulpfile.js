2dfab1a1e00e189d5bf311902fa252d52a8a2655
Sean Metzgar <sean.metzgar@gmail.com>
2017-12-11 11:57:41 -0500

    Syncing current dev version

---
+++ gulpfile.js
@@ -0,0 +1,270 @@
+/* globals require */
+"use strict";
+
+//Build Plugins
+var gulp = require("gulp");
+var pipe = require("gulp-pipe");
+var autoprefixer = require("gulp-autoprefixer");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+var sass = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+var concat = require("gulp-concat");
+var stripStyleComments = require("gulp-strip-css-comments");
+var stripComments = require("gulp-strip-comments");
+var cssmin = require("gulp-cssmin");
+var clean = require("gulp-clean");
+
+//Deployment Plugins
+var gutil  = require('gulp-util');
+var argv   = require('minimist')(process.argv);
+var gulpif = require('gulp-if');
+var prompt = require('gulp-prompt');
+var rsync  = require('gulp-rsync');
+var rsyncwrapper = require("rsyncwrapper");
+
+//Helper Functions
+module.exports = function pipe(stream, tubes) {
+  tubes = tubes || stream.slice(1);
+  return tubes.reduce(function(stream, tube) { return stream.pipe(tube); }, Array.isArray(stream) ? stream[0] : stream);
+};
+
+var getDependentTasks = function (taskName) {
+    var tasks = gulp.tasks;
+    var deps = [];
+    var task = null;
+    for (task in tasks) {
+
+        if (task.startsWith(taskName + ":")) {
+            deps.push(task);
+        }
+    }
+    return deps;
+};
+
+var throwError = function (taskName, msg) {
+  throw new gutil.PluginError({
+      plugin: taskName,
+      message: msg
+    });
+};
+
+//File Arrays
+var paths = {
+    js: {
+        vendor: [
+            "./src/vendor/signals/signals.js",
+            "./src/vendor/jquery/jquery.js",
+            "./src/vendor/hasher/hasher.js",
+            "./src/vendor/crossroads/crossroads.js",
+            "./src/vendor/popper/popper.js",
+            "./src/vendor/bootstrap/js/bootstrap.js",
+            "./src/vendor/bootstrap-slider/js/bootstrap-slider.js"
+        ],
+        src:[
+            "./src/js/src/oembed.js",
+            "./src/js/src/scripts.js",
+            "./src/js/src/routes.js"
+        ]
+    },
+    sass: {
+        vendor: [
+            "./src/vendor/bootstrap/scss/bootstrap.scss"
+        ],
+        src: [
+            "./src/scss/style.scss"
+        ],
+        bootstrap: [
+            ".src/scss/inc/bootstrap/**/*.scss"
+        ],
+        watch: [
+            "./src/scss/**/*.scss"
+        ]
+    },
+    php: {
+        src: [
+            "./src/**/*.php",
+            "./src/**/*.html"
+        ]
+    },
+    src: "./src",
+    dist: "./dist/",
+    clean: [
+        "./dist",
+        "./src/vendor",
+        "./src/css/all.min.css",
+        "./src/css/all.min.css.map",
+        "./src/js/all.js",
+        "./src/js/all.js.map"
+    ]
+}
+
+//Vendor Tasks
+gulp.task("vendor:jquery", function () {
+    return pipe(gulp.src("./node_modules/jquery/dist/jquery.js"),
+        [gulp.dest("./src/vendor/jquery")])
+        .on('error', gutil.log);
+});
+
+gulp.task("vendor:signals", function () {
+    return pipe(gulp.src("./node_modules/signals/dist/signals.js"),
+        [gulp.dest("./src/vendor/signals")])
+        .on('error', gutil.log);
+});
+
+gulp.task("vendor:hasher", function () {
+    return pipe(gulp.src("./node_modules/hasher/dist/js/hasher.js"),
+        [gulp.dest("./src/vendor/hasher")])
+        .on('error', gutil.log);
+});
+
+gulp.task("vendor:crossroads", function () {
+    return pipe(gulp.src("./node_modules/crossroads/dist/crossroads.js"),
+        [gulp.dest("./src/vendor/crossroads")])
+        .on('error', gutil.log);
+});
+
+gulp.task("vendor:slideout", function () {
+    return pipe(gulp.src("./node_modules/slideout/dist/slideout.js"),
+        [gulp.dest("./src/vendor/slideout")])
+        .on('error', gutil.log);
+});
+
+gulp.task("vendor:popper", function () {
+    return pipe(gulp.src("./node_modules/popper.js/dist/umd/popper.js"),
+        [gulp.dest("./src/vendor/popper")])
+        .on('error', gutil.log);
+});
+
+gulp.task("vendor:bootstrap:sass", function () {
+    return pipe(gulp.src(["./node_modules/bootstrap/scss/**/*.*", "!./node_modules/bootstrap/scss/_variables.scss", "./src/scss/inc/bootstrap/_variables.scss"]),
+        [gulp.dest("./src/vendor/bootstrap/scss")])
+        .on('error', gutil.log);
+});
+gulp.task("vendor:bootstrap:js", function () {
+     return pipe(gulp.src("./node_modules/bootstrap/dist/js/bootstrap.js"),
+        [gulp.dest("./src/vendor/bootstrap/js")])
+        .on('error', gutil.log);
+});
+
+gulp.task("vendor:bootstrap-slider:sass", function () {
+    return pipe(gulp.src("./node_modules/bootstrap-slider/src/sass/**/*.*"),
+        [gulp.dest("./src/vendor/bootstrap-slider/scss")])
+        .on('error', gutil.log);
+});
+gulp.task("vendor:bootstrap-slider:js", function () {
+     return pipe(gulp.src("./node_modules/bootstrap-slider/dist/bootstrap-slider.js"),
+        [gulp.dest("./src/vendor/bootstrap-slider/js")])
+        .on('error', gutil.log);
+});
+
+//Build Tasks
+gulp.task("sass", ["move"], function () {
+    return pipe(gulp.src(paths.sass.vendor.concat(paths.sass.src)),
+        [
+            sourcemaps.init(),
+            sass().on('error', gutil.log),
+            autoprefixer(),
+            concat("all.css"),
+            stripStyleComments({preserve: false}),
+            // cssmin(),
+            rename({suffix: '.min'}),
+            sourcemaps.write("./"),
+            gulp.dest("./src/css")
+        ])
+        .on('error', gutil.log);
+});
+
+gulp.task("js", ["move"], function () {
+    return pipe(gulp.src(paths.js.vendor.concat(paths.js.src)),
+        [
+            // sourcemaps.init(),
+            concat("all.js"),
+            stripComments(),
+            // uglify().on('error', gutil.log),
+            // sourcemaps.write("./"),
+            gulp.dest("./src/js")
+        ])
+        .on('error', gutil.log);
+});
+
+gulp.task("clean", function () {
+    return pipe(gulp.src(paths.clean, {read: false}),
+        [
+            clean()
+        ])
+        .on('error', gutil.log);
+});
+gulp.task("clean:dist", function () {
+    return pipe(gulp.src(paths.dist, {read: false}),
+        [
+            clean()
+        ])
+        .on('error', gutil.log);
+});
+
+gulp.task("build", ["clean:dist", "sass", "js"], function () {
+    return pipe(gulp.src([paths.src + "/**/*.*", paths.src + "/.htaccess"]),
+        [
+            gulp.dest(paths.dist)
+        ])
+        .on('error', gutil.log);
+});
+
+/* gulp.task("deploy", ["clean:dist", "build"], function () {
+    var rsyncPaths = [paths.dist];
+    // Default options for rsync
+    var rsyncConf = {
+        progress: true,
+        root: "dist",
+        incremental: true,
+        relative: true,
+        emptyDirectories: true,
+        recursive: true,
+        clean: true,
+        exclude: [".htaccess"],
+        chmod: 755,
+        hostname: process.env.FUZZY_TOOLS_SSH_HOST,
+        username: process.env.FUZZY_TOOLS_SSH_USER,
+        destination: argv.production ? process.env.FUZZY_TOOLS_SSH_PROD_PATH : process.env.FUZZY_TOOLS_SSH_DEV_PATH
+    };
+
+    return pipe(gulp.src(rsyncPaths),
+        [
+            gulpif(
+                argv.production,
+                prompt.confirm({
+                    message: "YO!! Are you sure you want to deploy to PRODUCTION?",
+                    default: false
+                })
+            ),
+            rsync(rsyncConf)
+
+        ])
+        .on('error', gutil.log);
+});
+
+gulp.task('deploy2', function() {
+  rsyncwrapper({
+    ssh: true,
+    src: paths.dist,
+    dest: process.env.FUZZY_TOOLS_SSH_USER + '@' + process.env.FUZZY_TOOLS_SSH_HOST + ':' + process.env.FUZZY_TOOLS_SSH_DEV_PATH,
+    recursive: true,
+    syncDest: true,
+    args: ['--verbose']
+  }, function(error, stdout, stderr, cmd) {
+      gutil.log(stdout);
+  });
+}); */
+
+//Watch Tasks
+gulp.task("watch", function () {
+    gulp.watch(paths.js.src, ["build"]);
+    gulp.watch(paths.sass.watch, ["build"]);
+    gulp.watch(paths.php.src, ["build"]);
+});
+
+//General Tasks
+gulp.task("move:vendor", getDependentTasks("vendor"));
+gulp.task("move", getDependentTasks("move"));
+gulp.task("default", ["build", "watch"]);
---
+++ src/index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Kingdom Care</title>
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,300i,400,400i,600,600i,700,700i|Josefin+Slab:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="css/all.min.css" rel="stylesheet">
+</head>
+
+<body>
+	<div id="app-wrapper">
+        <div class="app-view home-view">
+        	<figure class="background-photo">
+        		<img src="img/photos/kc_home_photo.jpg">
+        	</figure>
+        	<div class="content-wrap">
+    			<div class="content">
+	    			<a href="/" class="logo-link" title="Kingdom Care"><h1 class="visuallyhidden">Kingdom Care</h1></a>
+
+	    			<div class="inner-content">
+		    			<p class="lead-in">Helping Christian parents connect with like-minded babysitters.</p>
+
+		    			<p class="mb-1"><em>Pre-register now to stay informed.</em></p>
+						<p>
+							<a href="/#/register/parent" class="btn btn-success btn-two-line">
+								<small class="font-weight-light"><em>I'm a parent...</em></small><br>
+								Help me find a sitter!
+							</a>
+							<a href="/#/register/sitter" class="btn btn-success btn-two-line">
+								<small class="font-weight-light"><em>I'm a sitter...</em></small><br>
+								Help me find work!
+							</a>
+						</p>
+					</div>
+				</div>
+
+				<footer>
+					<p class="mb-1"><em>
+						Need a little more info<span class="long">rmation before you get started</span>?<br class="long">
+						<span class="medium long">No problem, you can learn more about Kingdom Care’s values <a href="/our-story" class="white">here</a>.</span>
+					</em></p>
+					<a href="/#/our-story" class="btn btn-white">Our Story</a>
+				</footer>
+        	</div>
+        </div>
+
+        <div class="app-view sitter-registration"></div>
+
+        <div class="app-view parent-registration"></div>
+
+        <div class="app-view thanks-registration">
+        	<header class="view-header">
+			    <a class="logo-link" href="/"><h1 class="visuallyhidden">Kingdom Care</h1></a>
+			</header>
+
+			<div class="content-wrap">
+				<div class="container content">
+					<div class="row">
+						<div class="col text-center align-self-center">
+							<h3>Thanks for signing up!</h3>
+							<p>We'll be in touch with you in the next couple days with instructions on how to access your profile.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+        </div>
+
+        <div class="app-view error-registration">
+        	<header class="view-header">
+			    <a class="logo-link" href="/"><h1 class="visuallyhidden">Kingdom Care</h1></a>
+			</header>
+
+			<div class="content-wrap">
+				<div class="container content">
+					<div class="row">
+						<div class="col text-center align-self-center">
+							<h3>Oops! Something went wrong.</h3>
+							<p>There was an error while processing your pre-registration. Please try again in a little bit.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+        </div>
+
+        <div class="app-view about">
+        	<header class="view-header">
+			    <a class="logo-link" href="/"><p class="visuallyhidden">Kingdom Care</p></a>
+			</header>
+
+			<div class="content-wrap">
+				<div class="container content">
+					<div class="row">
+						<div class="col text-center align-self-center">
+							<h1>Our Story</h1>
+							<p>...</p>
+						</div>
+					</div>
+				</div>
+			</div>
+        </div>
+    </div>
+
+    <script type="text/javascript" src="js/all.js"></script>
+</body>
+</html>
